02132db2d0d8624b0a2a891a43b469327b712ed5
Johannes Klose <jklose@calitrix.com>
2023-06-20 17:50:52 +0200

    fix: mobile input bar size

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ export const Layout: FC<{ children?: ReactNode; footer?: ReactNode }> = ({
     <div className="relative h-full mx-auto max-w-4xl sm:p-6 lg:p-8 bg-white border-gray-200 dark:bg-gray-800">
       {children}
       {footer && (
-        <div className="absolute bottom-0 sm:left-6 lg:left-8 sm:right-6 lg:right-8 z-50  h-16">
+        <div className="absolute bottom-4 left-4 right-4 bottom-0 sm:left-6 lg:left-8 sm:right-6 lg:right-8 z-50 ">
           {footer}
         </div>
       )}
